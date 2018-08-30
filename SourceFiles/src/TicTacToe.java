9ce9313de143f00b5a6e7bc4b8466b370d75f9c2
NicholsTC1928 <NicholsTC1928@aguafria.org>
2018-08-30 09:05:09 -0700

    Added an optional Tic-Tac-Toe WIP program.

---
+++ SourceFiles/src/TicTacToe.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class TicTacToe
+{
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        boolean useAI = false;
+        System.out.println("Welcome to Tic-Tac-Toe! This was programmed by Tyler Nichols.");
+        System.out.print("How many players will be participating? (Enter either a \"1\" or a \"2.\") ");
+        boolean numCheck = true;
+        int pCount = reader.nextInt();
+        while(numCheck){
+            if(pCount != 1 && pCount != 2){
+                System.out.println("Invalid Input");
+                System.out.print("How many players will be participating? (Enter either a \"1\" or a \"2.\") ");
+                int pCountEdit = reader.nextInt();
+                pCount = pCountEdit;
+            }
+            else numCheck = false;
+        }
+        String winner;
+        if(pCount == 1){
+            useAI = true;
+            System.out.print("Enter Your Name: ");
+            String name = reader.next(); //Winner declared as "winSolo"
+            String nameAI = "CPU"; //Winner declared as "winAI"
+            winner = gameAI(name, nameAI);
+        }
+        else{
+            System.out.print("Enter the Name of Player 1: ");
+            String name1 = reader.next(); //Winner declared as "winP1"
+            System.out.print("Enter the Name of Player 2: ");
+            String name2 = reader.next(); //Winner declared as "winP2"
+            winner = game(name1, name2);
+        }
+
+
+    }
+
+    public static String gameAI(String x, String y){
+
+    }
+
+    public static String game(String x, String y){
+
+    }
+}
