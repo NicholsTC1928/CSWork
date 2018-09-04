27c75610c76194e836a505ab5b15cdc3cd8c51ba
NicholsTC1928 <NicholsTC1928@aguafria.org>
2018-09-04 09:05:59 -0700

    Updated the Tic-Tac-Toe program.

--- SourceFiles/src/TicTacToe.java
+++ SourceFiles/src/TicTacToe.java
@@ -19,32 +19,61 @@ public class TicTacToe
             }
             else numCheck = false;
         }
-        String winner;
+        //String winner;
         if(pCount == 1){
-            useAI = true;
-            System.out.print("Enter Your Name: ");
-            String name = reader.next(); //Winner declared as "winSolo"
-            String nameAI = "CPU"; //Winner declared as "winAI"
-            winner = gameAI(name, nameAI);
+            gameAI();
         }
         else{
-            System.out.print("Enter the Name of Player 1: ");
-            String name1 = reader.next(); //Winner declared as "winP1"
-            System.out.print("Enter the Name of Player 2: ");
-            String name2 = reader.next(); //Winner declared as "winP2"
-            winner = game(name1, name2);
+            game();
         }
 
 
     }
 
-    public static String gameAI(String x, String y){ //Make 2-Player Game First
-
+    public static void gameAI(){ //Make 2-Player Game First
+        System.out.println("This mode has not been implemented yet. Sorry for the inconvenience! :)");
     }
 
-    public static String game(String x, String y){
+    public static void game(){
+        Scanner reader = new Scanner(System.in);
         Random rand = new Random();
         int startP = rand.nextInt(2) + 1; //2 is the maximum random integer, and 1 is the minimum.
+        String currentP;
+        int move = 1; //Declares whose move it is - Update as moves are completed.
+        int useX;
+        int useO;
+        System.out.print("Enter the Name of Player 1: ");
+        String name1 = reader.next(); //Winner declared as "winP1"
+        System.out.print("Enter the Name of Player 2: ");
+        String name2 = reader.next(); //Winner declared as "winP2"
+        if(startP == 1){
+            currentP = 1;
+            //move = 1;
+            useX = 1;
+            //useO = 2;
+            System.out.println(name1 + " will make the first move.");
+        }
+        else {
+            currentP = 2;
+            //move = 2;
+            useX = 2;
+            //useO = 1;
+            System.out.println(name2 + " will make the first move.");
+        }
+        System.out.println("To access the help information, type in \"help\" during a move.");
+        System.out.println();
+        System.out.println("   1  2  3");
+        System.out.println("1  -  -  -");
+        System.out.println("2  -  -  -");
+        System.out.println("3  -  -  -");
+        System.out.println();
+        boolean gameIsRunning = true;
+        while(gameIsRunning){
+            if (currentP = useX){
+                
+            }
+        }
+        System.out.print(currentP + " (X) - Enter the name of the space that you wish to claim. ");
 
     }
 }
