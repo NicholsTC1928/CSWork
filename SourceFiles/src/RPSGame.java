d011f578123f8b54f55d7b9b0b7724016c5e58c5
NicholsTC1928 <NicholsTC1928@aguafria.org>
2018-10-31 10:08:21 -0700

    Updated the RPS game. (10/31)

--- SourceFiles/src/RPSGame.java
+++ SourceFiles/src/RPSGame.java
@@ -1,13 +1,25 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Random;
 public class RPSGame {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Rock Paper Scissors! You will be playing against the computer in this game.");
-        boolean go = true;
-        while(go){
-            go = game();
+        System.out.println("Welcome to Rock Paper Scissors!");
+        //The following lines of code combine Exercises 1 and 3 into one program.
+        System.out.print("Would you like to play a game against the computer (1), or compare the stats of 2 players (2)? ");
+        int choice = input.nextInt();
+        while(choice != 1 && choice != 2){
+            System.out.println("Invalid Input");
+            System.out.print("Would you like to play a game against the computer (1), or compare the stats of 2 players (2)? ");
+            choice = input.nextInt();
         }
+        if(choice == 1){
+            boolean go = true;
+            while(go){
+                go = game();
+            }
+        }
+        else compare();
     }
 
     public static boolean game(){
@@ -15,7 +27,7 @@ public class RPSGame {
         Random r = new Random();
         String moveP = "null";
         while(!moveP.equals("rock") && !moveP.equals("paper") && !moveP.equals("scissors")){ //Making sure the input is valid
-            System.out.print("Enter either \"rock,\" \"paper,\" or \"scissors\" to make your move.");
+            System.out.print("Enter either \"rock,\" \"paper,\" or \"scissors\" to make your move. ");
             moveP = input.next().toLowerCase();
             if(!moveP.equals("rock") && !moveP.equals("paper") && !moveP.equals("scissors")) System.out.println("Invalid Input");
         }
@@ -41,9 +53,31 @@ public class RPSGame {
             else System.out.println(cpuWin);
         }
         if(moveP.equals(moveCPU)) System.out.println("Since both are the same choice, this round is a draw."); //Both are the same
-        System.out.print("Would you like to play again? (Enter a \"y\" to continue, or any other key to quit.)");
+        System.out.print("Would you like to play again? (Enter \"y\" to start a new round, or any other key to quit.) ");
         String cont = input.next().toLowerCase();
-        if(cont.equals("y")) return true;
+        if(cont.equals("y")) return true; //Checks to see if the player wants to start a new round
         else return false;
+    }
+
+    public static void compare(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the name of the first person here. ");
+        String name1 = input.next();
+        System.out.print("Enter the name of the second person here. ");
+        String name2 = input.next();
+        System.out.print("How many times has " + name1 + " played Rock Paper Scissors? ");
+        int play1 = check();
+
+    }
+
+    public static int check(){
+        Scanner input = new Scanner(System.in);
+        int a;
+        for(;;){
+           a = input.nextInt();
+           if(input.hasNextInt()) break;
+           else System.out.print("Invalid Input");
+       }
+       return a;
     }
 }
