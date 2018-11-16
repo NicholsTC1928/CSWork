8d80478595892fa7a530a76499471247642b1872
NicholsTC1928 <NicholsTC1928@aguafria.org>
2018-11-16 09:06:57 -0700

    Added the Snake program. (11/16)

---
+++ SourceFiles/src/snake.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.awt.*;
+
+public class snake {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to (yet another game of) Snake! This was programmed by Tyler Nichols.");
+        //String difficulty;
+        System.out.print("Which difficulty do you want to play at? (1 - Easy / 2 - Normal / 3 - Hard / 4 - Mania) ");
+        try{
+           int dChoice = input.nextInt();
+        }
+        catch(InputMismatchException e){
+            System.out.println("Invalid Input - Please enter a numeric value between 1 and 4 corresponding to the desired difficulty.");
+            System.out.print("Which difficulty do you want to play at? (1 - Easy / 2 - Normal / 3 - Hard / 4 - Mania) ");
+            int dChoice = input.nextInt();
+        }
+        System.out.println(dChoice); //Debug
+    }
+
+    public static void game(){
+
+    }
+
+    public static int difficulty(){
+        Scanner input = new Scanner(System.in);
+        for(;;){
+            
+        }
+    }
+}
