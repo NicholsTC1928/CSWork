336ebac5819cbf7bfcbc87e8382079d65297bffb
NicholsTC1928 <NicholsTC1928@aguafria.org>
2018-11-20 08:17:50 -0700

    Updated the Snake program. (11/20)

--- SourceFiles/src/snake.java
+++ SourceFiles/src/snake.java
@@ -17,33 +17,30 @@ public class snake {
         Scanner input = new Scanner(System.in);
         int inCheck;
         try{
-          inCheck = input.nextInt();
+            inCheck = input.nextInt();
         }
-        catch(InputMismatchException e){            
-          //input.close();
-          return -1;
+        catch(InputMismatchException e){
+            return -1;
         }
-        //input.close();
         return inCheck;
      }
-
-     public static int difficultyChoice(){
+    public static int difficultyChoice(){
         Scanner dInput = new Scanner(System.in);
         int dChoice = -1;
         for(;;){
-          System.out.print("Which difficulty do you want to play at? (1 - Easy / 2 - Normal / 3 - Hard / 4 - Mania) ");
-          dChoice = checkInput();
-          if(1 <= dChoice && 4 >= dChoice) break;
-          System.out.println("Invalid Input - Please enter only a \"1,\" \"2,\" \"3,\" or a \"4.\"");
+            System.out.print("Which difficulty do you want to play at? (1 - Easy / 2 - Normal / 3 - Hard / 4 - Mania) ");
+            dChoice = checkInput();
+            if(1 <= dChoice && 4 >= dChoice) break;
+            System.out.println("Invalid Input - Please enter only a \"1,\" \"2,\" \"3,\" or a \"4.\"");
         }
         String[] diffList = {"Easy","Normal","Hard","Mania"};
         if(dChoice == 4){
-          System.out.print("Wait, you actually chose Mania?! Are you sure that you want to play on this difficulty? It is not even remotely fair! (Enter a \"Y\" to continue, or any other key to choose a new difficulty.) ");
-          String mConfirm = dInput.next().toLowerCase();
-          if(mConfirm.equals("y")) return dChoice;
-          else return -1;
-        } 
+            System.out.print("Wait, you actually chose Mania?! Are you sure that you want to play on this difficulty? It is not even remotely fair! (Enter a \"Y\" to continue, or any other key to choose a new difficulty.) ");
+            String mConfirm = dInput.next().toLowerCase();
+            if(mConfirm.equals("y")) return dChoice;
+            else return -1;
+        }
         System.out.println("You have chosen " + diffList[dChoice - 1] + " as your difficulty.");
         return dChoice;
-     }
+    }
 }
