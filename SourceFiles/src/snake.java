80a0b056c647ed0d3fdb45a86e4fae5fc9f3b33f
NicholsTC1928 <30838339+NicholsTC1928@users.noreply.github.com>
2018-11-20 08:06:50 -0700

    Add files via upload
    Updated the Snake program. (11/20)

--- SourceFiles/src/snake.java
+++ SourceFiles/src/snake.java
@@ -5,16 +5,7 @@ public class snake {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to (yet another game of) Snake! This was programmed by Tyler Nichols.");
-        //String difficulty;
-        System.out.print("Which difficulty do you want to play at? (1 - Easy / 2 - Normal / 3 - Hard / 4 - Mania) ");
-        try{
-           int dChoice = input.nextInt();
-        }
-        catch(InputMismatchException e){
-            System.out.println("Invalid Input - Please enter a numeric value between 1 and 4 corresponding to the desired difficulty.");
-            System.out.print("Which difficulty do you want to play at? (1 - Easy / 2 - Normal / 3 - Hard / 4 - Mania) ");
-            int dChoice = input.nextInt();
-        }
+        int dChoice = difficultyChoice();
         System.out.println(dChoice); //Debug
     }
 
@@ -22,10 +13,37 @@ public class snake {
 
     }
 
-    public static int difficulty(){
+    public static int checkInput(){
         Scanner input = new Scanner(System.in);
-        for(;;){
-            
+        int inCheck;
+        try{
+          inCheck = input.nextInt();
         }
-    }
+        catch(InputMismatchException e){            
+          //input.close();
+          return -1;
+        }
+        //input.close();
+        return inCheck;
+     }
+
+     public static int difficultyChoice(){
+        Scanner dInput = new Scanner(System.in);
+        int dChoice = -1;
+        for(;;){
+          System.out.print("Which difficulty do you want to play at? (1 - Easy / 2 - Normal / 3 - Hard / 4 - Mania) ");
+          dChoice = checkInput();
+          if(1 <= dChoice && 4 >= dChoice) break;
+          System.out.println("Invalid Input - Please enter only a \"1,\" \"2,\" \"3,\" or a \"4.\"");
+        }
+        String[] diffList = {"Easy","Normal","Hard","Mania"};
+        if(dChoice == 4){
+          System.out.print("Wait, you actually chose Mania?! Are you sure that you want to play on this difficulty? It is not even remotely fair! (Enter a \"Y\" to continue, or any other key to choose a new difficulty.) ");
+          String mConfirm = dInput.next().toLowerCase();
+          if(mConfirm.equals("y")) return dChoice;
+          else return -1;
+        } 
+        System.out.println("You have chosen " + diffList[dChoice - 1] + " as your difficulty.");
+        return dChoice;
+     }
 }
