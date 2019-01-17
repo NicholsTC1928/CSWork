4fe06a3a9641848ace456f09b74058671841b7ec
NicholsTC1928 <30838339+NicholsTC1928@users.noreply.github.com>
2019-01-17 11:54:22 -0700

    Updated the Chapter 8 Project. (1/17)

--- SourceFiles/src/Chapter8Project/RationalNumber.java
+++ SourceFiles/src/Chapter8Project/RationalNumber.java
@@ -10,17 +10,15 @@ public class RationalNumber {
         //be reduced (?).
         int newNum = numerator;
         int newDen = denominator;
-        while((Math.abs(newNum) % Math.abs(newDen)) == 0){
-            int greatestNum;
-            if(Math.abs(numerator) >= Math.abs(denominator)) greatestNum = Math.abs(numerator);
-            else greatestNum = Math.abs(denominator);
-            int gcf = 1; //All values have a gcf of 1.
-            for(int i = 1; i <= greatestNum; i++){
-                if((Math.abs(numerator) % i) == 0 && (Math.abs(denominator) % i == 0)) gcf = i;
-            }
-            newNum /= gcf;
-            newDen /= gcf;
+        int greatestNum;
+        if(Math.abs(numerator) >= Math.abs(denominator)) greatestNum = Math.abs(numerator);
+        else greatestNum = Math.abs(denominator);
+        int gcf = 1; //All values have a gcf of 1.
+        for(int i = 1; i <= greatestNum; i++){
+            if((Math.abs(numerator) % i) == 0 && (Math.abs(denominator) % i == 0)) gcf = i;
         }
+        newNum /= gcf;
+        newDen /= gcf;
         if((newDen < 0 && newNum > 0) || (newDen < 0 && newNum < 0)){
             newNum *= -1;
             newDen *= -1;
@@ -45,5 +43,33 @@ public class RationalNumber {
         if(this.denominator == 1) return Integer.toString(this.numerator);
         if(this.numerator == 0) return Integer.toString(0);
         return (this.numerator + " / " + this.denominator);
+    }
+
+    public RationalNumber add(RationalNumber r){
+        int newNumThis = this.numerator;
+        int newDenThis = this.denominator;
+        int newNumR = r.numerator;
+        int newDenR = r.denominator;
+        if(this.denominator != r.denominator){
+           int temp = this.denominator;
+           newNumThis = this.numerator * r.denominator;
+           newDenThis = this.denominator * r.denominator;
+           newNumR = r.numerator * temp;
+           newDenR = r.denominator * temp;
+        }
+        return (new RationalNumber((newNumThis + newNumR),newDenThis));
+    }
+
+    public RationalNumber subtract(RationalNumber r){
+        RationalNumber rTemp = new RationalNumber((r.numerator * -1),r.denominator);
+        return this.add(rTemp);
+    }
+
+    public RationalNumber multiply(RationalNumber r){
+        return (new RationalNumber((this.numerator * r.numerator),(this.denominator * r.denominator)));
+    }
+
+    public RationalNumber divide(RationalNumber r){
+        return (new RationalNumber((this.numerator * r.denominator),(this.denominator * r.numerator)));
     }
 }
