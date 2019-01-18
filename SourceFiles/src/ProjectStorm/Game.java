88dacd5a3919b98c6a33dd3beaa8752028a815dc
NicholsTC1928 <30838339+NicholsTC1928@users.noreply.github.com>
2019-01-18 08:05:02 -0700

    Updated Project Storm. (1/18)

--- SourceFiles/src/ProjectStorm/Game.java
+++ SourceFiles/src/ProjectStorm/Game.java
@@ -12,6 +12,13 @@ public class Game extends JPanel implements Runnable {
     Thread runnerAnim;
     volatile boolean running = true;
     private final int DELAY = 25;
+    int totalFramesCount = 0;
+    Timer timerForFPS = new Timer();
+    TimerTask updateFPS = new TimerTask(){
+        public void run(){
+            repaint(0,0,40,10); //This paints only the part of the screen displaying the frame rate counter.
+        }
+    }
 
     public Game(){
         initGameBoard();
@@ -31,7 +38,17 @@ public class Game extends JPanel implements Runnable {
 
     @Override public void paintComponent(Graphics g){
         super.paintComponent(g);
+        drawFPS(totalFramesCount,g);
         Toolkit.getDefaultToolkit().sync();
+    }
+    
+    private void drawFPS(int totalFramesCount, Graphics g){
+        int alpha = 127; //This variable controls transparency, with 127 being ~50% of the total (255).
+        Color FPSRectangle = new Color(0,0,0,alpha); //This creates a black color with 50% transparency.
+        g.setColor(FPSRectangle);
+        g.fillRect(0,0,40,10);
+        g.setColor(Color.GREEN);
+        g.drawString(totalFramesCount.toString(),0,0);
     }
 
     private void startThreadForAnimation(){
@@ -48,7 +65,8 @@ public class Game extends JPanel implements Runnable {
     @Override public void run(){
         long beforeTime, timeDiff, sleep;
         beforeTime = System.currentTimeMillis();
-        while(runnerAnim != null){
+        //Animation Loop (Main Game Loop?)
+        while(true){
             repaint();
             timeDiff = System.currentTimeMillis() - beforeTime;
             sleep = DELAY - timeDiff;
@@ -58,6 +76,7 @@ public class Game extends JPanel implements Runnable {
             }
             catch(InterruptedException e){}
             beforeTime = System.currentTimeMillis();
+            totalFramesCount++;
         }
     }
 
