69a2c2c09b52d11ac74c156875d49c8de3cbb32c
NicholsTC1928 <NicholsTC1928@aguafria.org>
2019-01-22 09:07:50 -0700

    Updated Project Storm. (1/22)

--- SourceFiles/src/ProjectStorm/Game.java
+++ SourceFiles/src/ProjectStorm/Game.java
@@ -6,24 +6,34 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.util.Timer;
 import java.util.TimerTask;
-//FPS Counter Link: https://stackoverflow.com/questions/20769767/calculate-fps-in-java-game
+/*
+The following PC port necessities HAVE BEEN properly implemented:
+    -FPS Counter
+    -Fullscreen Mode
 
+The following PC port necessities NEED TO BE properly implemented:
+    -Key Bindings
+    -FPS Cap
+*/
 public class Game extends JPanel implements Runnable {
-    Thread runnerAnim;
+    private Thread runnerAnim;
     volatile boolean running = true;
     private final int DELAY = 25;
-    int totalFramesCount = 0;
-    Timer timerForFPS = new Timer();
-    TimerTask updateFPS = new TimerTask(){
+    private int totalFramesCount = 0;
+    private int framesToDisplay = 0;
+    private Timer timerForFPS = new Timer();
+    private TimerTask updateFPS = new TimerTask(){
         @Override public void run(){
-            repaint(0,0,40,10); //This paints only the part of the screen displaying the frame rate counter.
+            framesToDisplay = totalFramesCount; //Setting a variable to display the current frame count every second
+            //makes sure that the counter is only printed once every second.
+            repaint(0,0,70,20); //This paints only the part of the screen displaying the frame rate counter rectangle.
             totalFramesCount = 0;
         }
     };
 
     public Game(){
         initGameBoard();
-        timerForFPS.scheduleAtFixedRate(updateFPS,1000,1000);
+        timerForFPS.scheduleAtFixedRate(updateFPS,0,1000);
         //Debug Methods - Mouse click prints mouse location to the console
         addMouseListener(new MouseAdapter(){
             public void mousePressed(MouseEvent e){
@@ -40,18 +50,25 @@ public class Game extends JPanel implements Runnable {
 
     @Override public void paintComponent(Graphics g){
         super.paintComponent(g);
-        
-        drawFPS(totalFramesCount,g);
+        drawFPSRect(g);
+        drawFPSCount(g);
     }
     
-    private void drawFPS(int totalFramesCount, Graphics g){
+    private void drawFPSRect(Graphics g){
         int alpha = 127; //This variable controls transparency, with 127 being ~50% of the total (255).
         Color FPSRectangle = new Color(0,0,0,alpha); //This creates a black color with 50% transparency.
-        g.setColor(FPSRectangle);
-        g.fillRect(0,0,40,10);
+        //g.setColor(FPSRectangle);
+        g.setColor(Color.RED);
+        g.fillRect(0,0,70,20);
         Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void drawFPSCount(Graphics g){
+        String currentFrameRate = this.framesToDisplay + " FPS";
+        Font fpsDisplay = new Font("Helvetica",Font.PLAIN,18);
         g.setColor(Color.GREEN);
-        g.drawString(totalFramesCount + " FPS",0,0);
+        g.setFont(fpsDisplay);
+        g.drawString(currentFrameRate,0,15);
         Toolkit.getDefaultToolkit().sync();
     }
 
@@ -76,7 +93,14 @@ public class Game extends JPanel implements Runnable {
             sleep = DELAY - timeDiff;
             if(sleep < 0) sleep = 2;
             try{
-                Thread.sleep(sleep);
+                Thread.sleep(2000);
+                /*
+                FPS Tips:
+                    -Time is proportional to (1 / FPS)
+                    -1 second (1000 ms) gives 2 FPS
+                    -.5 seconds (500 ms) gives 4 FPS
+                    -.25 seconds (250 ms) gives 8 FPS
+                */
             }
             catch(InterruptedException e){}
             beforeTime = System.currentTimeMillis();
