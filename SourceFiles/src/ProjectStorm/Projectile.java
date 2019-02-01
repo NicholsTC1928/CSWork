89fc76af6f81e4c382ebf43376b1e78873852c70
NicholsTC1928 <30838339+NicholsTC1928@users.noreply.github.com>
2019-02-01 11:50:52 -0700

    Update Projectile.java

--- SourceFiles/src/ProjectStorm/Projectile.java
+++ SourceFiles/src/ProjectStorm/Projectile.java
@@ -1,5 +1,63 @@
 package ProjectStorm;
 
 public class Projectile{
-
+    private double speedX;
+    private final double initialSpeedX;
+    private double speedY;
+    private final double initialSpeedY;
+    private double currentXPos;
+    private double currentYPos;
+    private boolean isExplosive;
+    
+    public Projectile(double speedX,double speedY,double currentPosX,double currentPosY,boolean isExplosive){
+        this.speedX = speedX;
+        this.initialSpeedX = speedX;
+        this.speedY = speedY;
+        this.initialSpeedY = speedY;
+        this.currentXPos = currentXPos;
+        this.currentYPos = currentYPos;
+        this.isExplosive = isExplosive;
+    }
+    
+    public double getSpeedX(){
+        return this.speedX;
+    }
+    
+    public void multiplySpeedXBy(double multiplier){
+        this.speedX *= multiplier;
+    }
+    
+    public double setSpeedX(new speed){
+        this.speedX = speed;
+    }
+    
+    public double getInitialSpeedX(){
+        return this.initialSpeedX;
+    }
+    
+    public double getSpeedY(){
+        return this.speedY;
+    }
+    
+    public void multiplySpeedYBy(double multiplier){
+        this.speedY *= multiplier;
+    }
+    
+    public double setSpeedY(new speed){
+        this.speedY = speed;
+    }
+    
+    public double getInitialSpeedY(){
+        return this.initialSpeedY;
+    }
+    
+    public double getCurrentXPos(){
+        return this.currentXPos;
+    }
+    
+    public void changeCurrentXPosBy(double speed){
+        this.currentXPos += speed;
+    }
+    
+    public void setCurrentXPos(
 }
