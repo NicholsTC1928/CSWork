95feea2a6ad2e1a8b28db1a981c1e9858f424388
NicholsTC1928 <30838339+NicholsTC1928@users.noreply.github.com>
2019-04-04 08:08:46 -0700

    Updated the Quiz Program. (4/4)

--- SourceFiles/src/QuizProgram/Quiz.java
+++ SourceFiles/src/QuizProgram/Quiz.java
@@ -244,21 +244,334 @@ public class Quiz {
         )));
         return quiz;
     }
-/*
+
     public static List<Question> quiz2(){
         //Standards 4-6
         List<Question> quiz = new ArrayList<Question>();
+        quiz.add(new Question(createQArray(
+            "In Java, which of the following is NOT a primitive data type?",
+            "String",
+            "integer",
+            "float",
+            "double"
+        )));
+        quiz.add(new Question(createQArray(
+            "In Java, if data is NOT a primitive type, then what type of data must it be?",
+            "object",
+            "function",
+            "method",
+            "conditional"
+        )));
+        quiz.add(new Question(createQArray(
+            "If you are writing a program to determine if a number is even, which of the following data types would allow a true response to be reported when the number is even?",
+            "boolean",
+            "numeric",
+            "String",
+            "integer"
+        )));
+        quiz.add(new Question(createQArray(
+            "If you are writing a program that will need to determine the number of characters in an input response, which of the following data types should be used to store the input?",
+            "String",
+            "integer",
+            "float",
+            "boolean"
+        )));
+        quiz.add(new Question(createQArray(
+            "If you are writing a program to store the quotient and remainder as separate whole numbers, which of the following data types should be used when declaring the variables?",
+            "integer",
+            "float",
+            "double",
+            "String"
+        )));
+        quiz.add(new Question(createQArray(
+            "What is wrong with the statement \"int a = 5.5;?\"",
+            "The value being assigned is a real number, while the data type is declared as an integer.",
+            "The value being assigned is an integer and the data type is an int.",
+            "The variable is not a valid variable name.",
+            "The variable cannot assign its data type and a value at the same time."
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of the following shows the correct syntax when writing an assignment statement?",
+            "variableName = assignment;",
+            "expression = variableName;",
+            "dataType = variableName;",
+            "dataType = expression;"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of the following cannot be used as a variable because it is a reserved word?",
+            "boolean",
+            "apple",
+            "2b",
+            "b2"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of the following correctly uses length on the String object (str) and stores the result correctly?",
+            "int len = str.length();",
+            "float len = str.length();",
+            "double len = str.size();",
+            "int len = str.length"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of the following correctly constructs a String variable (str) to store the String \"Hello World!?\"",
+            "String str = \"Hello World!\";",
+            "\"Hello World!\" = String str;",
+            "String(\"Hello World!\")",
+            "String str(\"Hello World!\");"
+        )));
+        quiz.add(new Question(createQArray(
+            "How many choices are possible when writing an if/else structure in your program?",
+            "two",
+            "one",
+            "three",
+            "four"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of the following operators would correctly determine that num1 is less than num2?",
+            "<",
+            ">",
+            "<>",
+            "="
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of the following operators would correctly determine if a number is less than or equal to a second number?",
+            "<=",
+            ">=",
+            "<>",
+            "="
+        )));
+        quiz.add(new Question(createQArray(
+            "Given a list of numbers that are ordered, which of the following operators could determine if the list is descending?",
+            ">=",
+            "<",
+            "<=",
+            "!="
+        )));
+        quiz.add(new Question(createQArray(
+            "What is the output of the following code fragment?\n int value = 6;\n if(value != 26) System.out.println(\"You missed!\");\n else System.out.println(\"You hit!\");\n System.out.println(\"Try again!\");",
+            "You missed! Try again!",
+            "You hit! Try again!",
+            "You missed! You hit!",
+            "You missed!Try again!"
+        )));
+        quiz.add(new Question(createQArray(
+            "What is the binary value of decimal 207?",
+            "11001111",
+            "11111001",
+            "11111111",
+            "11110000"
+        )));
+        quiz.add(new Question(createQArray(
+            "How many megabytes are in a gigabyte?",
+            "1000 MB",
+            "2000 MB",
+            "500 MB",
+            "200 MB"
+        )));
+        quiz.add(new Question(createQArray(
+            "What is the purpose of order of operation in software programming?",
+            "To determine which mathematical operation to perform first",
+            "To determine which mathematical operations are valid",
+            "To determine how many mathematical operations are required",
+            "To determine which mathematical operations are NOT valid"
+        )));
+        quiz.add(new Question(createQArray(
+            "How does the ++ symbol in programming change a variable?",
+            "Add 1 to the value of itself",
+            "Subtract 1 from the value of itself",
+            "Multiply by 2 to the value of itself",
+            "Divide by 2 to the value of itself"
+        )));
+        quiz.add(new Question(createQArray(
+            "How does the -- symbol in programming change a variable?",
+            "Subtract 1 from the value of itself",
+            "Add 1 to the value of itself",
+            "Multiply by 2 to the value of itself",
+            "Divide by 2 to the value of itself"
+        )));
+        quiz.add(new Question(createQArray(
+            "How would you show the value of a added to the value of b?",
+            "a + b",
+            "a - b",
+            "a * b",
+            "a / b"
+        )));
+        quiz.add(new Question(createQArray(
+            "How would you show the value of a subtracted by the value of b?",
+            "a - b",
+            "a + b",
+            "a / b",
+            "a * b"
+        )));
+        quiz.add(new Question(createQArray(
+            "How would you show the value of a divided by the value of b?",
+            "a / b",
+            "a * b",
+            "a - b",
+            "a + b"
+        )));
+        quiz.add(new Question(createQArray(
+            "How would you show the value of a multiplied by the value of b?",
+            "a * b",
+            "a / b",
+            "a + b",
+            "a - b"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of the following is a valid integer?",
+            "-25",
+            "0.25",
+            "0.5",
+            "0.75"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of the following is a valid integer?",
+            "5",
+            "0.25",
+            "0.1",
+            "0.57"
+        )));
+        quiz.add(new Question(createQArray(
+            "What comes first in the if/else statement?",
+            "if",
+            "else",
+            "not",
+            "then"
+        )));
+        quiz.add(new Question(createQArray(
+            "What does the == symbol in programming mean?",
+            "Equals (Comparison)",
+            "Subtraction",
+            "Assignment",
+            "Multiplication"
+        )));
+        quiz.add(new Question(createQArray(
+            "What does the = symbol in programming mean?",
+            "Assignment",
+            "Subtraction",
+            "Addition",
+            "Equals (Comparison)"
+        )));
+        quiz.add(new Question(createQArray(
+            "What does the && symbol in programming mean?",
+            "And",
+            "Not",
+            "Or",
+            "Equals"
+        )));
     }
 
     public static List<Question> quiz3(){
         //Standards 8 and 9
         List<Question> quiz = new ArrayList<Question>();
+        quiz.add(new Question(createQArray(
+            "How do you declare a variable to store 10 different integer values in VB?",
+            "Dim numbers[9] as Integer",
+            "Dim numbers[10] as Integer",
+            "Dim numbers as IntegerArray",
+            "Dim numbers[1 to 10] as Integer"
+        )));
+        quiz.add(new Question(createQArray(
+            "How do you declare a variable to store 10 different integer values in Java?",
+            "int[] numbers = new int[10];",
+            "int[10] numbers;",
+            "int numbers[10];",
+            "int[10] numbers[10];"
+        )));
+        quiz.add(new Question(createQArray(
+            "What operation is used to put a set of values in an array in order?",
+            "Sort",
+            "List",
+            "Order",
+            "Organize"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of the following is NOT a sorting algorithm?",
+            "FIFO",
+            "Bubble",
+            "Insertion",
+            "Merge"
+        )));
+        quiz.add(new Question(createQArray(
+            "What is the most efficient sorting algorithm?",
+            "Merge",
+            "Bubble",
+            "Insertion",
+            "FIFO"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which sorting algorithm is most efficient based on O?",
+            "O(log(n))",
+            "O(n)",
+            "O(n^2)",
+            "O(2n)"
+        )));
+        quiz.add(new Question(createQArray(
+            "What is a common syntax error?",
+            "error: ';' expected",
+            "divide by zero error",
+            "unhandled exception",
+            "off by one error"
+        )));
+        quiz.add(new Question(createQArray(
+            "What is the difference between http and https?",
+            "https encrypts the data sent between the browser and server",
+            "https provides spell-checking capability",
+            "https allows for a larger packet size",
+            "There is no difference between the two"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of these is an advantage of IPv6 over IPv4?",
+            "IPv6 allows for a larger number of IP addresses",
+            "IPv6 is faster than IPv4",
+            "IPv6 checks to make sure the IP address is properly formatted",
+            "IPv6 uses 6-bit addresses, while IPv4 uses 4-bit addresses"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of the following protocols would be used to send e-mail?",
+            "IMAP",
+            "FTP",
+            "HTTP",
+            "HTTPM"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of these represents the amount of time it takes for a packet to reach a destination?",
+            "Latency",
+            "Bandwidth",
+            "Packet size",
+            "Baud rate"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of these represents the amount of dtat transferred during a set period of time?",
+            "Bandwidth",
+            "Latency",
+            "Packet size",
+            "TTL"
+        )));
+        quiz.add(new Question(createQArray(
+            "What is the purpose of DNS?",
+            "To associate an IP address with a hostname",
+            "Provide encryption services",
+            "Route packets over large distances",
+            "Provide language translation between countries"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of these protocols works along with TCP to provide internet connectivity?",
+            "Internet Protocol (IP)",
+            "HTTP",
+            "FTP",
+            "IMAP"
+        )));
+        quiz.add(new Question(createQArray(
+            "Which of these features is not guaranteed by the TCP/IP protocols?",
+            "No packet will be dropped",
+            "Error checking",
+            "All packets will be delivered in the same order as they were sent",
+            "The delivery of all packets"
+        )));
     }
 
     public static List<Question> quiz4(){
         //Standards 12, 16, 18, and 19
         List<Question> quiz = new ArrayList<Question>();
     }
-
- */
 }
